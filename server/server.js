8d3b22b4251f0865b2f9a6f42182eac5a1280e4d
GunnySensei <gunnerwagoner@hotmail.com>
2022-03-31 16:17:43 -0500

    folder restructure with server, concurrently

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -30,6 +31,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
